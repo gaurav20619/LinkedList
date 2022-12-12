9ad64a89515f7a54b391f3e711fd46dc6f2a3a28
gaurav20619 <gauravsinghraijada619@gmail.com>
2022-12-12 19:53:22 +0530

    Add: program to create linked list by adding at end.

--- src/main/java/com/bridgelabz/linkedlist/Main.java
+++ src/main/java/com/bridgelabz/linkedlist/Main.java
@@ -1,8 +1,20 @@
 package com.bridgelabz.linkedlist;
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-		System.out.println(" Welcome toLinkedList DataStructure.");
-		Operation.addDataAtStart();
+		System.out.println("Welcome to LinkedList DataStructure Program :");
+		Scanner scan = new Scanner(System.in);
+        System.out.println("Enter 1 to add the data at the start"); // UC 2
+        System.out.println("Enter 2 to add the data at the End"); // UC 3
+        switch (scan.nextInt()) {
+
+            case 1:
+                Operation.addDataAtStart();
+                break;
+            case 2:
+                Operation.addDataAtEnd();
+                break;
 	}
+        scan.close();
+	}    
 }
