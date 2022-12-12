1f2b5cbea7c362327c7dd47a7a66316e24cf1e13
gaurav20619 <gauravsinghraijada619@gmail.com>
2022-12-12 19:36:28 +0530

    Add: program to create linked list by adding at start.

---
+++ src/main/java/com/bridgelabz/linkedlist/Main.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.linkedlist;
+
+public class Main {
+    public static void main(String[] args) {
+		System.out.println(" Welcome toLinkedList DataStructure.");
+		Operation.addDataAtStart();
+	}
+}
